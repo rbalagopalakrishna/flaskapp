8cb653891a2c7634097a0deb03e04cd258b95121
rbalagopalakrishna <balagopalraj@gmail.com>
2019-09-23 04:23:49 +0000

    unittests

--- test_app.py
+++ test_app.py
@@ -8,13 +8,24 @@ class TestHello(unittest.TestCase):
         self.app = app.app.test_client()
 
     # test 1
-    def test_hello_hello(self):
+    def test_hello_hello_a(self):
         rv = self.app.get('/')
         self.assertEqual(rv.status, '200 OK')
         self.assertEqual(rv.data, b'Hello Bala!')
 
     #test 2
-    def test_hello(self):
+    def test_hello_a(self):
+        result = app.hello()
+        self.assertEqual(result, 'Hello Bala!')
+
+    # test 3
+    def test_hello_hello_b(self):
+        rv = self.app.get('/')
+        self.assertEqual(rv.status, '200 OK')
+        self.assertEqual(rv.data, b'Hello Bala!')
+
+    #test 4
+    def test_hello_b(self):
         result = app.hello()
         self.assertEqual(result, 'Hello Bala!')
 
